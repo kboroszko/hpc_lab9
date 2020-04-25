d4b8bb9c141c0a71b4e1da4f770bf096387cb128
kboroszko <kajetan.boroszko@gmail.com>
2020-04-25 12:11:56 +0200

    rows moze

--- laplace-par.cpp
+++ laplace-par.cpp
@@ -88,8 +88,8 @@ static std::tuple<int, double> performAlgorithm(int myRank, int numProcesses, Gr
             int rows = frag->lastRowIdxExcl - frag->firstRowIdxIncl;
             double* shared_row_top = frag->data[otherColor][0];
             double* my_row_top = frag->data[otherColor][1];
-            double* shared_row_bottom = frag->data[otherColor][rows];
-            double* my_row_bottom = frag->data[otherColor][rows-1];
+            double* shared_row_bottom = frag->data[otherColor][rows - (color % 2)];
+            double* my_row_bottom = frag->data[otherColor][rows-1 - (color % 2)];
 
             if(myRank == 0){
                 MPI_Send(my_row_bottom, frag->gridDimension, MPI_DOUBLE, myRank+1, 0, MPI_COMM_WORLD );
@@ -197,13 +197,8 @@ static std::tuple<int, double> performAlgorithm(int myRank, int numProcesses, Gr
                 0,
                 MPI_COMM_WORLD
         );
-        if(myRank == 0){
-            std::cout << "iter=" << numIterations << "\tnode0=" << maxDiff ;
-        }
-        MPI_Barrier(MPI_COMM_WORLD);
-        if(myRank != 0){
-            std::cout << "\tnode1=" << maxDiff << "\n";
-        }
+            std::cout << "node" << myRank <<"  iter=" << numIterations << "\tnode0=" << maxDiff << "\n" ;
+
         MPI_Barrier(MPI_COMM_WORLD);
         maxDiff = globalMaxDiff;
     } while (maxDiff > epsilon && numIterations < 100);
