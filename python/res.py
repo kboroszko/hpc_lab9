9ab9f28c8ff7a519eb901e7b67c5428f3b639ad7
kboroszko <kajetan.boroszko@gmail.com>
2020-04-26 19:43:04 +0200

    python file

--- python/res.py
+++ python/res.py
@@ -1,17 +1,38 @@
 import numpy as np
 from matplotlib import pyplot as plt
 #%%
-seq = np.genfromtxt('results/seq/all.dat', delimiter='\t')
-noopt2  = np.genfromtxt('results/noopt/par2.err', delimiter='\t')
-noopt5  = np.genfromtxt('results/noopt/par5.err', delimiter='\t')
-noopt12 = np.genfromtxt('results/noopt/par12.err', delimiter='\t')
-noopt24 = np.genfromtxt('results/noopt/par24.err', delimiter='\t')
-noopt48 = np.genfromtxt('results/noopt/par48.err', delimiter='\t')
+seq = np.genfromtxt('data/seq/all.dat', delimiter='\t')
 
-noopt2_async  = np.genfromtxt('results/async/par2.err', delimiter='\t')
+noopt  = [np.genfromtxt('data/noopt/par{}.err'.format(i), delimiter='\t') for i in [2,5,12,24,48] ]
+allred  = [np.genfromtxt('data/allred/par{}.err'.format(i), delimiter='\t') for i in [2,5,12,24,48] ]
+asyn  = [np.genfromtxt('data/async/par{}.err'.format(i), delimiter='\t') for i in [2,5,12,24,48] ]
+
 #%%
 plt.plot(seq[:,0], seq[:, 1], label='seq')
-plt.plot(noopt2[:, 0], noopt2[:, 2], label='noopt2')
-plt.plot(noopt2_async[:, 0], noopt2_async[:, 2], label='noopt2_async')
+# nodes = [2,5,12,24,48]
+# for i in range(len(noopt)):
+#     arr = noopt[i]
+#     lab = "noopt{}".format(nodes[i])
+#     plt.plot(arr[:, 0], arr[:, 2], label=lab)
+plt.plot(noopt[0][:, 0], noopt[0][:, 2], label='n=2')
+plt.plot(noopt[1][:, 0], noopt[1][:, 2], label='n=5')
+plt.plot(noopt[3][:, 0], noopt[3][:, 2], label='n=24')
+plt.legend()
+plt.show()
+
+#%%
+
+n1000 = np.genfromtxt('data/noopt/n1000.dat', delimiter='\t')
+
+#%%
+plt.figure()
+plt.plot(n1000[:,0], n1000[:, 1])
+plt.plot()
+
+#%%
+plt.plot(seq[:,0], seq[:, 1], label='seq')
+# plt.plot(noopt[0][:, 0], noopt[0][:, 2], label='noopt n=2')
+# plt.plot(asyn[0][:, 0], asyn[0][:, 2], label='async n=2')
+plt.plot(allred[0][:, 0], allred[0][:, 2], label='allred n=2')
 plt.legend()
 plt.show()
